095f7db06dd5c1cdbffbac58d1a22e1862c1adf2
filatov04 <filka04012004@yandex.ru>
2025-02-08 01:09:33 +0300

    fix 2.0

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import './app.scss';
 import './src/widget/progressBar.js';
 import { progressBar } from './src/widget/progressBar.js';
+import './app.scss';
 
 document.querySelector('#app').innerHTML = `
   <main id='main'>
